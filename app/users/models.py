f0879101e1e997b4208e8f2adcd3b34f29bd09bf
vuesp27 <114942546+vuesp27@users.noreply.github.com>
2022-12-16 21:55:27 -0600

    changes to app

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=10, blank=False, default='')
+    password = models.CharField(max_length=10, blank=False, default='')
+    firstname = models.CharField(max_length=50, blank=True, default='')
+    lastname = models.CharField(max_length=50, blank=False, default='')
+    email = models.CharField(max_length=50, blank=False, default='')
+
+    # username = request.POST['username']
+#         password = request.POST['password']
+#         firstname = request.POST['firstname']
+#         lastname = request.POST['lastname']
+#         email = request.POST['email']
