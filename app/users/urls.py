72b21b43da262c6af7885ee0e00cd4f881187a5e
vuesp27 <114942546+vuesp27@users.noreply.github.com>
2022-12-16 22:28:09 -0600

    changes..

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,30 +6,12 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.urls import path
-from users import views as users_views
-from django.conf.urls.static import static
-from django.conf import settings
-
-# urlpatterns = [
-#     path('register/', user_views.register, name='register'),
-#     path('login/',
-#          auth_views.LoginView.as_view(template_name='users/login.html'),
-#          name='login'),
-#     path('logout/',
-#          auth_views.LogoutView.as_view(template_name='users/logout.html'),
-#          name='logout'),
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns = [
-    path('api/users/', user_views.register, name='add'),
-    path('api/login/', user_views.register),
-    path('api/logout/', user_views.register)
+    path('register/', user_views.register, name='register'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='users/login.html'),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='users/logout.html'),
+         name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# urlpatterns = [
-#     path('', tutorials_views.index.as_view(), name='home'),
-#     path('api/tutorials/', tutorials_views.tutorial_list),
-#     path('api/tutorials/<int:pk>/', tutorials_views.tutorial_detail),
-#     path('api/tutorials/published/', tutorials_views.tutorial_list_published)
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/users/views.py
+++ app/users/views.py
@@ -8,94 +8,49 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import BadHeaderError
 from django.template import loader
 
-
-from django.shortcuts import render
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from users.models import User
-from users.serializers import UserSerializer
-from rest_framework.decorators import api_view
-
 # Create your views here.
 User = get_user_model()
 
 
-# def register(request):
-#     next = request.GET.get('next', '/')
-#     try:
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         auth_user = authenticate(request, username=username, password=password)
-#         try:
-#             login(request, auth_user)
-#             return HttpResponseRedirect(next)
-#         except:
-#             messages.error(request, 'Invalid credentials')
-#             return HttpResponseRedirect(next)
-#     except (KeyError):
-#         messages.error(request, 'Invalid credentials')
-#         # return render(request, '/', { 'message': "Invalid username or password. Please try again." })
-
-
-@api_view(['GET', 'POST', 'DELETE'])
 def register(request):
-    if request.method == 'GET':
-        users = User.objects.all()
+    next = request.GET.get('next', '/')
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+        auth_user = authenticate(request, username=username, password=password)
+        try:
+            login(request, auth_user)
+            return HttpResponseRedirect(next)
+        except:
+            messages.error(request, 'Invalid credentials')
+            return HttpResponseRedirect(next)
+    except (KeyError):
+        messages.error(request, 'Invalid credentials')
+        # return render(request, '/', { 'message': "Invalid username or password. Please try again." })
 
-        user = request.GET.get('username', None)
-        if user is not None:
-            users = users.filter(name__icontains=user)
 
-        users_serializer = UserSerializer(users, many=True)
-        return JsonResponse(users_serializer.data, safe=False)
-        # 'safe=False' for objects serialization
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
 
-    elif request.method == 'POST':
-        user_data = JSONParser().parse(request)
-        user_serializer = UserSerializer(data=user_data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse(user_serializer.data,
-                                status=status.HTTP_201_CREATED)
-        return JsonResponse(user_serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+        print("----------------- register called")
+        print(username)
+        print(password)
+        print(firstname)
+        print(lastname)
+        print(email)
 
-    elif request.method == 'DELETE':
-        count = User.objects.all().delete()
-        return JsonResponse(
-            {
-                'message':
-                '{} Users were deleted successfully!'.format(count[0])
-            },
-            status=status.HTTP_204_NO_CONTENT)
+        # try:
+        #     user = get_object_or_404(User, username=username)
+        # except:
+        #     pass
 
-# def register(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         firstname = request.POST['firstname']
-#         lastname = request.POST['lastname']
-#         email = request.POST['email']
-
-#         print("----------------- register called")
-#         print(username)
-#         print(password)
-#         print(firstname)
-#         print(lastname)
-#         print(email)
-
-#         # try:
-#         #     user = get_object_or_404(User, username=username)
-#         # except:
-#         #     pass
-
-#         messages.success(request,
-#                          f'Your account has been created! You can now login!')
-#         return redirect('login')
-#     else:
-#         return render(request, 'users/register.html')
+        messages.success(request,
+                         f'Your account has been created! You can now login!')
+        return redirect('login')
+    else:
+        return render(request, 'users/register.html')
